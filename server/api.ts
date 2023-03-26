a643bc828f8fc3d62fa64b38d145ec4d83a0219a
AnxietyisReal <96593068+AnxietyisReal@users.noreply.github.com>
2023-03-26 17:17:15 +1100

    internally broken.

--- server/api.ts
+++ server/api.ts
@@ -1,21 +1,24 @@
 import utility from './libraries/utility';
 import logger from './libraries/logger';
+import {version} from './libraries/version';
 import config from '../config.json';
 import {Config} from '../typings/config';
 import {Server,Slots} from './model/server';
 import Game from './model/game';
+import axios from 'axios';
 
 config as Config;
-const uaString = 'Toasty/';
-const RequestTimeout = AbortSignal.timeout(5000);
+const uaString = `Toasty ${version}/`;
+const RequestTimeout = 8000;
+const Axios = axios.get;
 
 async function fetchMap(cb){
-  let map = await fetch(config.Livemap.Map,{signal:RequestTimeout,headers:{'User-Agent':`${uaString+'Map'}`}}).catch((err:Error)=>logger.error(err.message))
+  const map = await Axios(config.Livemap.Map,{timeout: RequestTimeout,headers:{'User-Agent':`${uaString+'Map'}`}}).catch((err:Error)=>logger.error(err.message))
   cb(map)
 }
 
 async function fetchXMLStats(cb){
-  let result = await fetch('http://'+config.FSServer.PanelURL+'/feed/dedicated-server-stats.json?code='+config.FSServer.APICode, {signal:RequestTimeout,headers:{'User-Agent':`${uaString+'DSS'}`}}).catch((err:Error)=>logger.error(err.message))
+  const result = await Axios('http://'+config.FSServer.PanelURL+'/feed/dedicated-server-stats.json?code='+config.FSServer.APICode, {timeout:RequestTimeout,headers:{'User-Agent':`${uaString+'DSS'}`}})
   cb(result)
 }
 
@@ -24,18 +27,16 @@ function fetchServerOnly(cb){
 }
 
 async function fetchEntities(cb){
-  fetchXMLStats((result)=>cb({
-    server: new Server(result.Server),
-    slots: new Slots(result.Server.Slots == undefined ? 0 : result.Server.Slots)
+  fetchXMLStats(async(result)=>cb({
+    server: console.log('server'), //new Server(await result.json().then(s=>s.server)),
+    slots: console.log(result.data) //new Slots((await result.json().then(s=>s.slots)) === undefined ? 0 : (await result.json().then(s=>s.slots)))
   }))
 }
 
 async function fetchCSG(cb){
-  let result = await fetch('http://'+config.FSServer.PanelURL+'/feed/dedicated-server-savegame.html?code='+config.FSServer.APICode+'&file=careerSavegame', {signal:RequestTimeout,headers:{'User-Agent':`${uaString+'CSG'}`}})//.catch((err:Error)=>logger.error(err.message))
-  const x = utility.c2json(result.body)
+  const result = await Axios('http://'+config.FSServer.PanelURL+'/feed/dedicated-server-savegame.html?code='+config.FSServer.APICode+'&file=careerSavegame', {timeout:RequestTimeout,headers:{'User-Agent':`${uaString+'CSG'}`}})
+  const x = utility.c2json(result.data)
   cb(new Game(x))
 }
 
-export default {
-  fetchMap, fetchServerOnly, fetchEntities, fetchCSG
-}
+export default {fetchMap, fetchServerOnly, fetchEntities, fetchCSG}
